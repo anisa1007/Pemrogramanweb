761e2d909775600cdc45a8f0715c74c916a9b469
anisa1007 <anisaraden25@gmail.com>
2020-11-19 01:37:08 -0600

    fisrt

---
+++ application/controllers/Auth.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+    public function index()
+    {
+        $this->load->view('login');
+    }
+    public function login()
+    {
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+
+        if ($email == "anisa@gmail.com" and $password == "akununila") {
+            $data = [
+                'status' => 'active'
+            ];
+            $this->session->set_userdata($data);
+            redirect('/Home');
+        } else {
+            redirect('/auth');
+        }
+    }
+    public function logout()
+    {
+        $this->session->unset_userdata('status');
+        redirect('/Home');
+    }
+}
---
+++ application/views/home.php
@@ -0,0 +1,2 @@
+<?php echo "Selamat datang Anisa Raden"; ?>
+<p>Klik disini untuk </p><a href="<?= base_url('auth'); ?>/logout">logout</a>
